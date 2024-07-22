fd55df11f540e5bb1408f89ef6531755465a80fe
Jinal Desai <58817990+jinaldesailive@users.noreply.github.com>
2024-07-22 12:00:04 +0100

    updated

--- fast-api-skeleton/content/main.py
+++ fast-api-skeleton/content/main.py
@@ -1,11 +1,28 @@
 from fastapi import FastAPI
 import uvicorn
+from google.cloud import spanner
+from fastapi.responses import HTMLResponse
+import json
 
 app = FastAPI()
+instance_id="appdb"
+database_id="tododb"
 
 @app.get("/hc/")
 def healthcheck():
     return 'Health - OK'
+
+@app.get("/tasks/")
+def get_db_data():
+    spanner_client = spanner.Client()
+    instance = spanner_client.instance(instance_id)
+    database = instance.database(database_id)
+
+    with database.snapshot() as snapshot:
+        results = snapshot.execute_sql(
+            "SELECT id, title, status FROM tasks"
+        )
+    return results.to_dict_list();
 
 @app.get("/")
 async def root():
