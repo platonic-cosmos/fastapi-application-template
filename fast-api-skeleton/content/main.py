85f51e0576c9c706f1194456db07fec56ae77539
sudhanshu-abhi-2 <sudhanshu.abhi2@gmail.com>
2024-07-25 12:00:15 +0530

    Update main.py

--- fast-api-skeleton/content/main.py
+++ fast-api-skeleton/content/main.py
@@ -6,7 +6,7 @@ import json
 
 app = FastAPI()
 instance_id="appdb"
-database_id="${{ values.spanner_db_name | dump }}"
+database_id="${{ values.spanner_db_name }}"
 
 @app.get("/hc/")
 def healthcheck():
